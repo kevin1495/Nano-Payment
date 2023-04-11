69591e5382dcf49ee01d946745f3ea9869dd2f79
Herry Adiwiguna <adiwiguna023@gmail.com>
2023-04-11 22:40:37 +0700

    first commit

---
+++ src/main/java/id/co/bca/spring/NanoPayment/NanoPaymentApplication.java
@@ -0,0 +1,13 @@
+package id.co.bca.spring.NanoPayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NanoPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NanoPaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/bca/spring/NanoPayment/NanoPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.bca.spring.NanoPayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NanoPaymentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
